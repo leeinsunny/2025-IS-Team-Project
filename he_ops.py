730526dc0c4631f2fe34af8f1a3cb6edf94fb474
eonpark <pocva6243@gmail.com>
2025-05-25 16:26:46 +0000

    fix: modified encrypt code

--- he_ops.py
+++ he_ops.py
@@ -1,24 +1,52 @@
-from heaan_stat import Context, HESeries
+from heaan_stat import Context, HESeries, Block
 
 #1. 동형암호 컨텍스트 생성 함수
 def make_context(key_dir="./keys"):
     return Context(key_dir_path=key_dir, generate_keys=False)
 
 #2. 암호문 상태에서 내적(dot product) 계산 함수
-def encrypted_dot(context, x, w):
+def encrypted_dot(context, x, w, b):
     # 1) 입력 벡터 암호화
-    x_he = HESeries(context, x, encrypt=True)
+    x_he = Block(context, data = x, encrypted=True)
+    print('입력벡터 암호화',x_he)
 
     # 2) 가중치 벡터 암호화
-    w_he = HESeries(context, w.tolist(), encrypt=True)
+    w_he = Block(context, data = w.tolist(), encrypted=True)
+    print('가중치 벡터 암호화',w_he)
 
-    # 3) 슬롯별 곱셈 (암호문 상태)
-    prod = x_he * w_he
-    
+    # bias float 암호화
+    b_he = Block(context, data =[b,0], encrypted=True)
+    print('bias 암호화',b_he)
+
+    #내적을 구현하기 위해서 곱셈 후 덧셈을 해야함 
+    prod_tmp =x_he * w_he
+    print('mult = ', prod_tmp)
+    print()
+    print()
+    print()
+    print()
+    # 3) 슬롯별 덧셈 (암호문 상태)
+    prod = prod_tmp.sum(axis=1, direction=0, unit_shape=(16, 2048))
+    print('sum = ',prod)
+    print()
+    print()
+    print()
+    print()
     # 4) 암호문 상태에서 합산 (예: 모든 슬롯 합산)
-    dot_product_encrypted = prod.sum()  # HEaaN에서 지원하는 암호문 합산 함수 사용
+    z_enc = prod + b_he
+    print(f"{b = }")
+    print()
+    print()
+    print()
+    print()
 
-    # 5) 평문 벡터 합산하여 dot product 반환
-    return dot_product_encrypted
+    print(f"{z_enc = }")
+    print()
+    print()
+    print()
+    print()
+ 
+
+    return z_enc
 
   
