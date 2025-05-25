a8736b4d0de6425c481d274728274950674e3b59
eonpark <pocva6243@gmail.com>
2025-05-25 02:35:00 +0000

    fix: add encrypt

--- he_ops.py
+++ he_ops.py
@@ -10,7 +10,7 @@ def encrypted_dot(context, x, w):
     x_he = HESeries(context, x, encrypt=True)
 
     # 2) 가중치 벡터 암호화
-    w_he = HESeries(context, w.tolist())
+    w_he = HESeries(context, w.tolist(), encrypt=True)
 
     # 3) 슬롯별 곱셈 (암호문 상태)
     prod = x_he * w_he
